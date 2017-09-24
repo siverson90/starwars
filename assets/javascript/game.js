c7cf32fce54262827f7d03e4416353c172201b50
Joshua Siverson <joshuasiverson@Joshuas-MacBook-Air.local>
2017-09-24 12:37:30 -0700

    changed logic for selecting characters

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,33 +1,29 @@
 
 var roasterList= {
 roaster1: {
-    name: "Verve",
-    hp: "100",
-    attack: "10",
-    defend: "5",
-    powerIncrement: "7"
+    hp: 100,
+    attack: 10,
+    defend: 5,
+    powerIncrement: 7
   },
 roaster2: {
-    name: "Phillz",
-    hp: "150",
-    attack: "5",
-    defend: "10",
-    powerIncrement: "5"
+    hp: 150,
+    attack: 5,
+    defend: 10,
+    powerIncrement: 5
   },
-"roaster3": {
-    name: "Blue Bottle",
-    hp: "120",
-    attack: "20",
-    defend: "10",
-    powerIncrement: "9",
+roaster3: {
+    hp: 120,
+    attack: 20,
+    defend: 10,
+    powerIncrement: 9,
   },
 
- "roaster4": {
-    name: "Sight Glass",
-    hp: "90",
-    attack: "5",
-    defend: "10",
-    powerIncrement: "12",
+ roaster4: {
+    hp: 90,
+    attack: 5,
+    defend: 10,
+    powerIncrement: 12,
 }
 };
 
@@ -35,10 +31,10 @@ roaster2: {
 var userHP="";
 var userAttack="";
 var userDefend="";
-var powerIncrementToUser="";
 var defenderHP="";
 var defenderAttack="";
 var defenderDefend="";
+var powerIncrementToUser="";
 var userId="";
 
 
@@ -51,14 +47,15 @@ function initalize(){
   var userHP=0;
   var userAttack=0;
   var userDefend=0;
-  var powerIncrementToUser=0;
 
   var defenderHP=0;
   var defenderAttack=0;
   var defenderDefend=0;
+  var powerIncrementToUser=0;
 
 }
 
+function unlockUser(){
 // User player and stats loaded on click
 $(".roaster").on("click", function() {
   // convert key to Int
@@ -68,63 +65,85 @@ $(".roaster").on("click", function() {
 
   console.log(userId);
   
-  findInObj(userId);
+  userFindInObj(userId);
 
 });
 
-function findInObj(keyValue){
+function userFindInObj(keyValue){
   
   var unlock = keyValue;
-  console.log(unlock);
   
-  console.log(roasterList[keyValue]);
+  var userCharacterObj=roasterList[keyValue];
+  
+  var result = Object.values(userCharacterObj);
+  // Need to grab values, change to Ints and assign to global variables
+  console.log(result);
+
+  userHP= result[0];
+  userAttack= result[1];
+  userDefend= result[2];
+
+  console.log("User HP" + userHP);
+  console.log("user attack" + userAttack);
+  console.log("User defend" + userDefend);
+
+ }
 };
 
+// Defender stats
+function unlockDefender(){
+$(".roaster").on("click", function() {
+  var defenderPick = ($(this).attr("id"));
 
+  var defenderId = defenderPick;
 
+  console.log(defenderId);
+  
+  defFindInObj(defenderId);
 
+function defFindInObj(keyValue){
+  
+  var unlock = keyValue;
+  
+  var userCharacterObj=roasterList[keyValue];
+  
+  var result = Object.values(userCharacterObj);
+  // Need to grab values, change to Ints and assign to global variables
 
+  console.log(result);
 
-// Defender stas
-// function unlockDefender(){
-// $(".roaster").on("click", function() {
-//   var defenderPick = $(this).attr("id");
+  defenderHP = result[0];
+  defenderAttack = result[1];
+  defenderDefend = result[2];
+  powerIncrementToUser= result[3];
 
-//   var defenderSelection = roasterList [ defenderPick ];
+  console.log("defend HP " + defenderHP);
+  console.log("defende attack " +defenderAttack);
+  console.log("defende defend " + defenderDefend);
+  console.log("amount to increase user " + powerIncrementToUser);
 
-//   var defendArr = Object.keys(defenderSelection).map(function (key) { return defenderSelection[key];});
-
-
-//   console.log(defendArr);
-
-//   defenderHP = (defendArr[1]);
-//   defenderAttack = (defendArr[2]);
-//   defenderDefend = (defendArr[3]);
-//   powerIncrementToUser= (defendArr[4]);
-
-//   console.log(defenderHP);
-//   console.log(defenderAttack);
-//   console.log(defenderDefend);
-//   console.log(powerIncrementToUser);
-// })
-// };
+    }
+  })
+};
 
 // *******Main content****
 // Insert Initialize function, calls userSelect which calls defenderSelect
 initalize();
 
+if()
+unlockUser();
+
+unlockDefender();
+
 $("#attackBtn").on("click", function(){
 
   // User Stats
   hpCounter= userHP-defenderDefend
-  attackIncrementerCounter = attackIncrementerCounter + powerIncrementToUser
-
 
   // Defender stats
   defenderCounter= defenderHP- userAttack
 
   console.log(hpCounter);
-  console.log(attackIncrementerCounter);
   console.log(defenderCounter);
 })
 
